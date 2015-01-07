eda28e44cdeab9f0df2a79d89140a307e127bdf5
Steven Slack <steven@s2webpress.com>
2015-01-06 21:33:28 -0500

    Initial Commit

---
+++ no-cart.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * No Cart
+ *
+ * @package   No_Cart
+ * @author    Steven Slack <steven@s2webpress.com>
+ * @license   GPL-2.0+
+ * @link      http://s2webpress.com/plugins/no-cart
+ * @copyright 2014 S2 Web LLC
+ *
+ * @wordpress-plugin
+ * Plugin Name:       No Cart Featured Products
+ * Plugin URI:        s2webpress.com/plugins/no-cart
+ * Description:       Add products to your site without having a shopping cart. That's right, this plugin has no shopping cart. This may be useful if you want to showcase products in your brick and morter location but not have to support an online store. Many of the same features as a regular shopping cart such as displaying the price, SKU, sale price, product categories and tags. 
+ * Version:           1.0.0
+ * Author:            S2 Web
+ * Author URI:        http://s2webpress.com
+ * Text Domain:       no-cart
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
