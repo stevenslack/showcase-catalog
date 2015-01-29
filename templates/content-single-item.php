936c2a3597594c3d5306d0dc12e332ba43354a9c
Steven Slack <steven@s2webpress.com>
2015-01-29 14:54:16 -0500

    single content template

---
+++ templates/content-single-item.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+ * Content for the single item view in No Cart
+ */
+
+?>
+
+	<div class="entry-content">
+		<h2>Yep!</h2>
+		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', '_s2' ) ); ?>
+	</div><!-- .entry-content -->
