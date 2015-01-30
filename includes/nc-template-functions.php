1595fd88f86ae1e6abccb3f2eb2f7aadbb64adfc
Steven Slack <steven@s2webpress.com>
2015-01-30 09:08:52 -0500

    Item content and image template hook

--- includes/nc-template-functions.php
+++ includes/nc-template-functions.php
@@ -36,3 +36,55 @@ if ( ! function_exists( 'nc_content_wrap_close' ) ) {
 		<?php
 	}
 }
+
+
+if ( ! function_exists( 'nc_single_image' ) ) {
+
+	/**
+	 * The single-content-item featured image
+	 * @return string
+	 */
+	function nc_single_image() {
+
+		if ( has_post_thumbnail() ) {
+		?>
+		<div class="item-image">
+			<?php the_post_thumbnail( array( 450, 450 ) ); ?>
+		</div>
+		<!-- /.item-image -->
+		<?php
+		}
+
+	}
+}
+
+
+if ( ! function_exists( 'nc_item_single_title' ) ) {
+
+	/**
+	 * Output the items title
+	 * 
+	 * @return string
+	 */
+	function nc_item_single_title() {
+		?>
+		<h1 itemprop="name" class="nc-item-title entry-title"><?php the_title(); ?></h1>
+		<?php
+	}
+
+}
+
+
+if ( ! function_exists( 'nc_item_content' ) ) {
+	
+	/**
+	 * No Cart Item Content
+	 * 
+	 * @return string the_content
+	 */
+	function nc_item_content() {
+		?>
+		<div class="nc-item-description"><?php the_content(); ?></div><!-- /.nc-item-description -->
+		<?php
+	}
+}
