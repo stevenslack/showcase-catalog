26f21a022e09fe0b2588852c2960cbb3bb172654
Steven Slack <steven@s2webpress.com>
2015-01-30 09:09:50 -0500

    removed dummy content and added hooked template

--- templates/content-single-item.php
+++ templates/content-single-item.php
@@ -5,7 +5,42 @@
 
 ?>
 
-	<div class="entry-content">
-		<h2>Yep!</h2>
-		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', '_s2' ) ); ?>
+	<?php do_action( 'no_cart_before_single_item' ); ?>
+
+	<div id="item-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+		<?php 
+			/**
+			 * Output the featured image
+			 * 
+			 * @hooked nc_single_image()
+			 */
+			do_action( 'no_cart_single_image' );
+		?>
+
+		<div class="item-details">
+
+			<?php 
+				/**
+				 * Output for the item details
+				 * 
+				 * @hooked nc_item_single_title()
+				 * @hooked nc_item_get_price()
+				 */
+				do_action( 'no_cart_item_details' );
+			?>
+
+		</div><!-- /.item-details -->
+
+		<?php
+			/**
+			 * No Cart Item Content
+			 * 
+			 * @hooked nc_item_content()
+			 */
+			do_action( 'no_cart_item_content' );
+		?>
+
 	</div><!-- .entry-content -->
+
+	<?php do_action( 'no_cart_after_single_item' ); ?>
